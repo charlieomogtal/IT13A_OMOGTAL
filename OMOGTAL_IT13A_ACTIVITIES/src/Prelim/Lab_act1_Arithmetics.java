bc9a71b6a478d780d4f2b1c9393d7fe968fd944b
CHARLIE <omogtalcharlie25@gmail.com>
2025-02-18 08:36:00 +0800

    added java application
    added hello world

---
+++ OMOGTAL_IT13A_ACTIVITIES/src/Prelim/Lab_act1_Arithmetics.java
@@ -0,0 +1,28 @@
+
+package Prelim;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_act1_Arithmetics {
+    public static void main(String[] args) {
+        
+      int first1= 10;
+      int second2 = 6;
+      int third3 = 4;
+      
+      
+      int addition = first1 + second2 + third3;
+      int subraction = first1 - second2 - third3;
+      int multiply = first1 * second2 * third3 ;
+      int divide = first1 / second2 / third3;
+      int module = first1 % second2 % third3;
+      
+      
+      System.out.println("  10 * 4 + 6  =" + addition);
+      System.out.println (" (  10-4) %  6 =" + subraction);
+      System.out.println ("( 10 / 4 + 6)/3 ="+  multiply);
+      System.out.println(" 10 *  6  -( 4 *  4)="  +  divide);
+    }
+}
