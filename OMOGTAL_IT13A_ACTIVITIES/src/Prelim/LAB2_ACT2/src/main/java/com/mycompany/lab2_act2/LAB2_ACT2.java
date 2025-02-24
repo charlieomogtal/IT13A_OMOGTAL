1794b8b929122d361d3f4dca4c56164f3a915913
charlieomogtal <omogtalcharlie25@gmail.com>
2025-02-24 18:15:50 +0800

    added java class

---
+++ OMOGTAL_IT13A_ACTIVITIES/src/Prelim/LAB2_ACT2/src/main/java/com/mycompany/lab2_act2/LAB2_ACT2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab2_act2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LAB2_ACT2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
