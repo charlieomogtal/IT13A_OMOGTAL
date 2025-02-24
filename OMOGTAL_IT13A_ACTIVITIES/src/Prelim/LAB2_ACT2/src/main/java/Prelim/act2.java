eb7f12b4b589d1ac94f715236f10b2366a4b777a
charlieomogtal <omogtalcharlie25@gmail.com>
2025-02-24 18:15:30 +0800

    added java class

---
+++ OMOGTAL_IT13A_ACTIVITIES/src/Prelim/LAB2_ACT2/src/main/java/Prelim/act2.java
@@ -0,0 +1,28 @@
+package Prelim;
+
+import java.util.Scanner;
+
+public class act2 {
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the First Number: ");
+            int num1 = input.nextInt();
+            System.out.print("Enter the Second Number: ");
+            int num2 = input.nextInt();
+            System.out.print("Enter the Third Number: ");
+            int num3 = input.nextInt();
+            
+            int greatest = num1;
+            
+            if (num2 > greatest) {
+                greatest = num2;
+            }
+            if (num3 > greatest) {
+                greatest = num3;
+            }
+            
+            System.out.print("The greatest number is: " + greatest);
+        }
+    }
+}
