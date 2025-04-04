09331f47dae88aae878f5df21f34cc5cb8f21eef
charlieomogtal <omogtalcharlie25@gmail.com>
2025-04-04 08:05:10 +0800

    added java class

--- OMOGTAL_IT13A_ACTIVITIES/Array_Activity.java
+++ OMOGTAL_IT13A_ACTIVITIES/Array_Activity.java
@@ -1,7 +1,45 @@
 
-package Midterm;
-
-
+//package Midterm;
+//
+//
 public class Array_Activity {
-    public static void main(string[] args){
+//    
+//    public static void main(String[] args) {
+//       int[][] array = {
+//           {1, 2, 3, 4},
+//           {5, 6, 7, 8},
+//           {9, 10, 11, 12}
+//           
+//       };
+//      
+//    
+//           for (int i = 0; i < array.length; i++){
+//               
+//    System.out.print("The elements in row" + (i + 1) + ":");
+//    for(int j = 0; j < array[i].length; j++)
+//    {
+//        System.out.print (array[i][j] + " ");
+//    }
+//        
+//     System.out.println();
+//    
+//       }
+//   }
+//               
+// }          
+//     
+//               
+//             
+//             
+//    
+//
+
+  public static void main (String[] args){
+      String[] array = {"A", "B", "C", "D", "F", "G"};
+      for (String letter : array){
+         System.out.print(letter + ",");
+      }
+  }
 }
+      
+    
